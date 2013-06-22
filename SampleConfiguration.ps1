3a05b3230ad4d2eb73a19cc4655547a7e3a94b89
Chris Duck <gpduck@whatsupduck.net>
2013-06-21 23:08:44 -0500

    Sample files
    These can be used with FakeFramework to actually run an (almost) DSC
    compatible configuration file.

---
+++ SampleConfiguration.ps1
@@ -0,0 +1,195 @@
+﻿Configuration DhcpServers {
+	Node $AllNodes {
+		WindowsFeature Dhcp @{
+			Ensure = "Present";
+			Name = "DHCP","RSAT-DHCP";
+		}
+		
+		DhcpV4SettingDSC DhcpSettings @{
+			Ensure = "Present";
+			Setting_ConflictDetectionAttempts = 1;
+			V4DnsSetting_DynamicUpdates = "Always";
+			V4DnsSetting_UpdateDnsRRForOlderClients = $true;
+			Requires = "[WindowsFeature]Dhcp";
+		}
+		
+		$Node.Classes | ForEach-Object {
+			$ClassInfo = $_
+			DhcpV4ClassDSC $ClassInfo.Name @{
+				Ensure = "Present";
+				Name = $ClassInfo.Name;
+				Data = $ClassInfo.Data;
+				"Type" = $ClassInfo.Type;
+				Description = $ClassInfo.Description;
+				Requires = "[DhcpV4SettingDSC]DhcpSettings";
+			}
+		}
+		
+		$Node.Options | ForEach-Object {
+			$OptionInfo = $_
+			DhcpV4OptionDefinitionDSC $OptionInfo.Name @{
+				Ensure = "Present";
+				Name = $OptionInfo.Name;
+				OptionId = $OptionInfo.OptionId;
+				"Type" = $OptionInfo.Type;
+				VendorClass = $OptionInfo.VendorClass;
+				Description = $OptionInfo.Description;
+				Requires = ($Node.Classes | %{ "[DhcpV4ClassDSC]$($_.Name)" })
+			}
+		}
+		
+		$Node.UserScopes | ForEach-Object {
+			$ScopeData = $_
+			#Create the scope
+			DhcpV4ScopeDSC $ScopeData.ScopeId @{
+				Ensure = "Present";
+				ScopeId = $ScopeData.ScopeId;
+				StartRange = $ScopeData.StartRange;
+				EndRange = $ScopeData.EndRange;
+				SubnetMask = $ScopeData.SubnetMask;
+				Name = $ScopeData.Name;
+				Description = $ScopeData.Description;
+				LeaseDuration = $Node.ClientLeaseDuration;
+				SuperscopeName = "Users";
+				Requires = "[WindowsFeature]Dhcp";
+			}
+			
+			#Configure DNS Server option
+			DhcpV4OptionValueDSC "$($ScopeData.ScopeId)_6" @{
+				Ensure = "Present";
+				OptionId = 6;
+				ScopeId = $ScopeData.ScopeId;
+				Value = $Node[$ScopeData.DnsKey];
+				Requires = "[DhcpV4ScopeDSC]$($ScopeData.ScopeId)";
+			}
+			
+			#Configure DnsDomain option
+			DhcpV4OptionValueDSC "$($ScopeData.ScopeId)_15" @{
+				Ensure = "Present";
+				OptionId = 15;
+				ScopeId = $ScopeData.ScopeId;
+				Value = $Node.DnsDomain;
+				Requires = "[DhcpV4ScopeDSC]$($ScopeData.ScopeId)";
+			}
+			
+			#Configure WinsServer option
+			DhcpV4OptionValueDSC "$($ScopeData.ScopeId)_44" @{
+				Ensure = "Present";
+				OptionId = 44;
+				ScopeId = $ScopeData.ScopeId;
+				Value = $Node.WinsServers;
+				Requires = "[DhcpV4ScopeDSC]$($ScopeData.ScopeId)";
+			}
+			
+			#Configure Wins Node Type option
+			DhcpV4OptionValueDSC "$($ScopeData.ScopeId)_46" @{
+				Ensure = "Present";
+				OptionId = 46;
+				ScopeId = $ScopeData.ScopeId;
+				Value = $Node.WinsNodeType;
+				Requires = "[DhcpV4ScopeDSC]$($ScopeData.ScopeId)";
+			}
+			
+			#Configure scope specific options
+			$ScopeData.Options | %{
+				$OptionData = $_
+				DhcpV4OptionValueDSC "$($ScopeData.ScopeId)_$($OptionData.OptionId)" @{
+					Ensure = "Present";
+					OptionId = $OptionData.OptionId;
+					ScopeId = $ScopeData.ScopeId;
+					Value = $OptionData.Value;
+					VendorClass = $OptionData.VendorClass;
+					UserClass = $OptionData.UserClass;
+					Requires = "[DhcpV4ScopeDSC]$($ScopeData.ScopeId)";
+				}
+			}
+		}
+		
+		$Node.ServerScopes | ForEach-Object {
+			$ScopeData = $_
+			#Create the scope
+			DhcpV4ScopeDSC $ScopeData.ScopeId @{
+				Ensure = "Present";
+				ScopeId = $ScopeData.ScopeId;
+				StartRange = $ScopeData.StartRange;
+				EndRange = $ScopeData.EndRange;
+				SubnetMask = $ScopeData.SubnetMask;
+				Name = $ScopeData.Name;
+				Description = $ScopeData.Description;
+				LeaseDuration = $Node.ServerLeaseDuration;
+				SuperscopeName = "Servers";
+				Requires = "[WindowsFeature]Dhcp";
+			}
+
+			#Configure NTP on our servers
+			DhcpV4OptionValueDSC "$($ScopeData.ScopeId)_4" @{
+				Ensure = "Present";
+				OptionId = 4;
+				ScopeId = $ScopeData.ScopeId;
+				Value = $Node.NtpServer;
+				Requires = "[DhcpV4ScopeDSC]$($ScopeData.ScopeId)";
+			}
+			
+			#Configure DNS Server option
+			DhcpV4OptionValueDSC "$($ScopeData.ScopeId)_6" @{
+				Ensure = "Present";
+				OptionId = 6;
+				ScopeId = $ScopeData.ScopeId;
+				Value = $Node.ProdDnsServers;
+				Requires = "[DhcpV4ScopeDSC]$($ScopeData.ScopeId)";
+			}
+			
+			#Configure DnsDomain option
+			DhcpV4OptionValueDSC "$($ScopeData.ScopeId)_15" @{
+				Ensure = "Present";
+				OptionId = 15;
+				ScopeId = $ScopeData.ScopeId;
+				Value = $Node.DnsDomain;
+				Requires = "[DhcpV4ScopeDSC]$($ScopeData.ScopeId)";
+			}
+			
+			#Configure WinsServer option
+			DhcpV4OptionValueDSC "$($ScopeData.ScopeId)_44" @{
+				Ensure = "Present";
+				OptionId = 44;
+				ScopeId = $ScopeData.ScopeId;
+				Value = $Node.WinsServers;
+				Requires = "[DhcpV4ScopeDSC]$($ScopeData.ScopeId)";
+			}
+			
+			#Configure Wins Node Type option
+			DhcpV4OptionValueDSC "$($ScopeData.ScopeId)_46" @{
+				Ensure = "Present";
+				OptionId = 46;
+				ScopeId = $ScopeData.ScopeId;
+				Value = $Node.WinsNodeType;
+				Requires = "[DhcpV4ScopeDSC]$($ScopeData.ScopeId)";
+			}
+			
+			#Configure scope specific options
+			$ScopeData.Options | %{
+				$OptionData = $_
+				DhcpV4OptionValueDSC "$($ScopeData.ScopeId)_$($OptionData.OptionId)" @{
+					Ensure = "Present";
+					OptionId = $OptionData.OptionId;
+					ScopeId = $ScopeData.ScopeId;
+					Value = $OptionData.Value;
+					VendorClass = $OptionData.VendorClass;
+					UserClass = $OptionData.UserClass;
+					Requires = "[DhcpV4ScopeDSC]$($ScopeData.ScopeId)";
+				}
+			}
+		}
+		
+		#We someone keeps adding a 4th floor, but we don't have one, so we want to make sure it gets deleted
+		DhcpV4ScopeDSC "PhantomFloor" @{
+			Ensure = "Absent";
+			ScopeId = "10.0.104.0";
+			#These are required for the module, but not used to actually remove the scope (so they don't have to match what is defined)
+			StartRange = "10.0.104.100";
+			EndRange = "10.0.104.101";
+			SubnetMask = "255.255.255.0";
+			Requires = "[WindowsFeature]Dhcp";
+		}
+	}
+}
---
+++ SampleConfigurationData.ps1
@@ -0,0 +1,181 @@
+@{
+	AllNodes = @(
+		@{
+			NodeName = "*";
+			ServerLeaseDuration = (New-TimeSpan -Days 15);
+			ClientLeaseDuration = (New-TimeSpan -Hours 12);
+			#Thx goog!
+			PublicDnsServers = "8.8.8.8","8.8.4.4";
+			ProdDnsServers = "10.1.0.10","10.1.0.11";
+			LabDnsServers = "10.2.0.10","10.2.0.11";
+			WinsServers = "10.1.0.10","10.1.0.11";
+			WinsNodeType = "0x08";
+			DnsDomain = "internal.contoso.com";
+			
+			#Region Classes
+				Classes = @(
+					@{
+						Name = "iPXE";
+						Data = "iPXE";
+						"Type" = "User";
+						Description = "User class for iPXE";
+					}, @{
+						Name = "MSUCClient";
+						"Type" = "Vendor";
+						Data = "MS-UC-Client";
+						Description = "UC Vendor Class Id";
+					}
+				);
+			#EndRegion Classes
+			
+			#Region Options
+				Options = @(
+					@{
+						Name = "UCSipServer";
+						OptionId = 120;
+						"Type" = "BinaryData";
+						Description = "Sip Server Fqdn";
+					}, @{
+						Name = "UCIdentifier";
+						OptionId = 1;
+						"Type" = "BinaryData";
+						VendorClass = "MSUCClient";
+						Description = "UC Identifier";
+					}, @{
+						Name = "URLScheme";
+						OptionId = 2;
+						"Type" = "BinaryData";
+						VendorClass = "MSUCClient";
+						Description = "URL Scheme";
+					}, @{
+						Name = "WebServerFqdn";
+						OptionId = 3;
+						"Type" = "BinaryData";
+						VendorClass = "MSUCClient";
+						Description = "Web Server Fqdn";
+					}, @{
+						Name = "WebServerPort";
+						OptionId = 4;
+						"Type" = "BinaryData";
+						VendorClass = "MSUCClient";
+						Description = "Web Server Port";
+					}, @{
+						Name = "CertProvRelPath";
+						OptionId = 5;
+						"Type" = "BinaryData";
+						VendorClass = "MSUCClient";
+						Description = "Cert Prov Relative Path";
+					}
+				);
+			#EndRegion Options
+			
+			#Region UserScopes
+				UserScopes = @(
+					@{
+						ScopeId = "10.0.101.0";
+						Name = 'HQ - 1st Floor';
+						StartRange = "10.0.101.100";
+						EndRange = "10.0.101.200";
+						SubnetMask = "255.255.255.0";
+						DnsKey = "ProdDnsServers";
+						Options = @(
+							#Router
+							@{ OptionId = 3; Value = "10.0.101.1"; }
+						)
+					}, @{
+						ScopeId = "10.0.102.0";
+						Name = "HQ - 2nd Floor";
+						StartRange = "10.0.102.100";
+						EndRange = "10.0.102.200";
+						SubnetMask = "255.255.255.0";
+						DnsKey = "ProdDnsServers";
+						Options = @(
+							#Router
+							@{ OptionId = 3; Value = "10.0.102.1"; }
+						)
+					}, @{
+						ScopeId = "10.0.103.0";
+						Name = 'HQ - 3rd Floor';
+						StartRange = "10.0.103.100";
+						EndRange = "10.0.103.200";
+						SubnetMask = "255.255.255.0";
+						DnsKey = "ProdDnsServers";
+						Options = @(
+							#Router
+							@{ OptionId = 3; Value = "10.0.103.1"; }
+						)
+					}, @{
+						ScopeId = "10.0.200.0";
+						Name = 'HQ - Wireless';
+						StartRange = "10.0.200.100";
+						EndRange = "10.0.201.254";
+						SubnetMask = "255.255.254.0";
+						DnsKey = "PublicDnsServers";
+						Options = @(
+							#Router
+							@{ OptionId = 3; Value = "10.0.200.1"; }
+						)
+					}
+				);
+			#EndRegion UserScopes
+			
+			#Region ServerScopes
+				ServerScopes = @(
+					@{
+						ScopeId = "10.1.0.0";
+						Name = 'Domain Controllers and Stuff';
+						Description = 'Its not like these can use DHCP anyway';
+						StartRange = "10.1.0.100";
+						EndRange = "10.1.0.105"
+						SubnetMask = "255.255.255.0";
+						Options = @(
+							#Router
+							@{ OptionId = 3; Value = "10.1.0.1"; }
+						)
+					}, @{
+						ScopeId = "10.1.1.0";
+						Name = 'Databases go here';
+						StartRange = "10.1.1.100";
+						EndRange = "10.1.1.150";
+						SubnetMask = "255.255.255.0";
+						Options = @(
+							#Router
+							@{ OptionId = 3; Value = "10.1.1.1"; }
+						)
+					}, @{
+						ScopeId = "10.1.2.0";
+						Name = 'Tier two is app I think';
+						StartRange = "10.1.2.100";
+						EndRange = "10.1.2.200";
+						SubnetMask = "255.255.255.0";
+						Options = @(
+							#Router
+							@{ OptionId = 3; Value = "10.1.2.1"; }
+						)
+					}, @{
+						ScopeId = "10.1.4.0";
+						Name = 'And three would be web';
+						Description = 'Apparently we have a lot of these';
+						StartRange = "10.1.4.100";
+						EndRange = "10.1.5.200";
+						SubnetMask = "255.255.254.0";
+						Options = @(
+							#Router
+							@{ OptionId = 3; Value = "10.1.4.1"; },
+							#And why not a PXE server
+							@{ OptionId = 66; Value = "10.1.0.10"; },
+							#And a file to boot
+							@{ OptionId = 67; Value = "\boot\x64\wdsnbp.com"; }
+						)
+					}
+				);
+			#EndRegion ServerScopes
+		}, @{
+			NodeName = "DhcpServer1";
+			NtpServer = "10.1.0.10";
+		}, @{
+			NodeName = "DhcpServer2";
+			NtpServer = "10.2.0.10";
+		}
+	);	
+}
